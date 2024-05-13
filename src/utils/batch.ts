052b136bd6740569c9b02d5565d110d62fb69669
Alex Khizhnyi <akhizhniy@spotware.com>
2024-05-13 11:16:13 +0300

    optimize subscription

--- src/utils/batch.ts
+++ src/utils/batch.ts
@@ -1,4 +1,20 @@
+import {v4} from 'uuid';
+
 // Default to a dummy "batch" implementation that just runs the callback
 export function defaultNoopBatch(callback: () => void) {
-  callback()
+  const channel = new MessageChannel();
+
+  const timeoutMessageName = `timeout-${v4()}`;
+
+  function handleMessage(event: MessageEvent) {
+    if (event.data === timeoutMessageName) {
+      event.stopPropagation();
+      callback();
+      channel.port1.close();
+      channel.port2.close();
+    }
+  }
+
+  channel.port1.onmessage = handleMessage;
+  channel.port2.postMessage(timeoutMessageName);
 }
